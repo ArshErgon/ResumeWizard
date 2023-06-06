3c7d8d0246a1d4ceb5970d3b39939b8a8d9d5f85
ArshErgon <arshergon@gmail.com>
2023-06-06 18:05:47 +0430

    chore: added Skills section and work experience

--- src/components/ResumeOne.svelte
+++ src/components/ResumeOne.svelte
@@ -7,10 +7,11 @@
     <br />
   </div>
   <div class="social-container">
-    <span class="socials" contenteditable="true"
-      >will this workwill this workwill this workwill this workwill this
-      workwill this workwill this workwill this work will this workwill this
-      workwill this work will this work</span
+    <span class="socials" contenteditable="true">
+    will this workwill this workwill this workwill this workwill this
+      work will this workwill this workwill this work will this workwill this
+      work will this work will this work
+      </span
     >
   </div>
   <div class="summary">
@@ -19,6 +20,23 @@
         this will be the summary site of the page chatgpt will help us alot of the problem we will be facing in the future of the world
     </div>
   </div>
+  
+  <div class="skills">
+      <div class="resume-heading">Skills</div>
+      <div class="skill-lists" id="skills" contenteditable="true">Python * Java * JavaScript * Rust * HTML</div>
+  </div>
+
+  <div class="work-experience">
+    <div class="resume-heading">Work Experience</div>
+    <!-- job title one -->
+    <div class="title-one">
+        <div style="display: flex;">
+            <div class="job-title">Programmer</div>
+            <div class="date">Jan to Dec</div>
+        </div>
+    </div>    
+  </div>
+
 </div>
 
 <style>
@@ -37,8 +55,7 @@
   }
 
   .summary {
-    margin-top: 50px;
-    width: 100%;
+    margin-top: 40px;
     background: rgba(255, 0, 0, 0.548);
   }
 
@@ -65,5 +82,29 @@
     color: black;
     border-bottom: 2px solid black;
     font-family: Cardo;
+    margin-top: 15px;
   }
+
+  .skills {
+    background-color: lightgrey;
+  }
+
+  .skill-lists {
+    margin: 5px 8px 0px 1pc;
+    color: black;
+  }
+
+.work-experience {
+    background: orangered;
+}
+
+.job-title {
+    margin: 5px 8px 0px 1pc;
+    color: black;
+}
+.date {
+    color: black;
+    margin: 5px 8px 0px 1pc;
+}
+
 </style>
