ba3ce23786609f2083c0893c5b5e102fd9d57d5f
ArshErgon <arshergon@gmail.com>
2023-06-07 20:00:41 +0430

    fix: remove the overwritten css and reformated the education section

---
+++ src/components/SideBarLeft.svelte
@@ -0,0 +1,21 @@
+<script>
+    
+  </script>
+  
+  <div class="sidebar-left">
+    <!-- Sidebar content -->
+  </div>
+  
+  <style>
+    .sidebar-left {
+      width: 400px;
+      height: 29.7cm;
+      background-color: #f0f0f0;
+      top: 5pc;
+      position: absolute;
+      border-right: 1px solid black;
+    }
+  
+
+  </style>
+  
---
+++ src/components/SideBarRight.svelte
@@ -0,0 +1,21 @@
+<script>
+
+</script>
+
+
+<div class="sidebar-right">
+    <!-- Sidebar content -->
+  </div>
+  
+  <style>
+    .sidebar-right {
+      width: 350px;
+      height: 29.7cm;
+      background-color: #f0f0f0;
+      top: 5pc;
+      position: absolute;
+      border-left: 1px solid black;
+    }
+  
+
+  </style>
