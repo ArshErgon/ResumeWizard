fd4cd3017a3eefe252d8f4562e5e463d8893c1ca
ArshErgon <arshergon@gmail.com>
2023-06-15 12:18:40 +0430

    formatting

--- src/lib/components/helper.js
+++ src/lib/components/helper.js
@@ -1,10 +1,9 @@
 export function titleCase(str) {
-    return str.replace(/\w\S*/g, (word) => {
-      return word[0].toUpperCase() + word.slice(1).toLowerCase();
-    });
-  }
+  return str.replace(/\w\S*/g, (word) => {
+    return word[0].toUpperCase() + word.slice(1).toLowerCase();
+  });
+}
 
-  export function replaceAsterisks(str) {
-    return str.replace(/\*/g, " • ");
-  }
-  
+export function replaceAsterisks(str) {
+  return str.replace(/\*/g, " • ");
+}
