450bdbbf32bcd8fb8c6638c8db1d215d0b25d265
ArshErgon <arshergon@gmail.com>
2023-06-19 19:14:07 +0430

    change btm-nav

--- src/lib/components/ResumeOne.svelte
+++ src/lib/components/ResumeOne.svelte
@@ -571,7 +571,6 @@
                 <em>Pro tip: You can change the color and size of your name.</em
                 >
                 <p class="ml-5 mt-3"><b>Name:</b> {header.name}</p>
-                <input type="text" bind:value={header.name} />
               </div>
           {:else if resumeFields.role}
             <p class="ml-5 mt-3"><b>Role:</b> {header.role}</p>
@@ -692,22 +691,31 @@
       ></div>
     </div>
   </div>
-  <div class="btm-nav">
-    <button class="text-info">
+  <div class="btm-nav" id="btm-nav">
+    <button class="text-info active" id="btm-nav-button">
       <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
-    </button>
-    <button class="text-info active">
-      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-    </button>
-    <button class="text-info">
-      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" /></svg>
     </button>
   </div>
 </div>
 
 <style>
+  #btm-nav {
+    width: 10%;
+    display: flex;
+    margin: 0px auto;
+    border-top-left-radius: 30px;
+    border-top-right-radius: 30px ;
+  }
+  #btm-nav-button {
+    border-top-left-radius: 30px;
+    border-top-right-radius: 30px ;
+
+  }
+
   em {
     margin: 15px 15px 0 15px;
+    padding: 0px 0px 3px 0px;
+    border-bottom: 1px solid #f5D300;
   }
   input {
     margin-bottom: 10px;
