513595b196ff1ffedb0d6b403990e90da83a1ba0
ArshErgon <arshergon@gmail.com>
2023-06-21 18:57:09 +0430

    added text comp

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
+import compression from 'compression';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,7 +7,13 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+
+		server: {
+			middleware: [
+			  compression(),
+			],
+		  },
 	}
 };
 
