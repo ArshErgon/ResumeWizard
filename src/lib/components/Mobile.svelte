8f1a0c7ebedd0bea609e9e2381a96f3e56edef07
ArshErgon <arshergon@gmail.com>
2023-06-22 12:44:43 +0430

    added mobile error page

---
+++ src/lib/components/Mobile.svelte
@@ -0,0 +1,17 @@
+<div class="sm:container">
+    <div class="container-custom">
+        <div class="ml-3 mr-3 text-[30px] text-center font-[500]">We are not ready for smaller devices.</div>
+        <div class="ml-3 mr-3"><br>
+            I'm really sorry but ResumeWizard isn't ready for mobile or smaller devices, as I need to completely the mobile site which can handle all the operations which the desktop site containing, it should be responsive and roboust it will take some time.
+            You can contribute to increase the speed of the development <a href="https://github.com/ArshErgon/ResumeWizard">ResumeWizard.</a>
+        </div>
+    </div>
+</div>
+
+<style>
+    .container-custom {
+        width: 100%;
+        height: 30px;
+        margin-top: 40px;
+    }
+</style>
